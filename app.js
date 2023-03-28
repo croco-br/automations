aa1986171b0eee56816af650be5a6187d1be03ee
croco <adrianomxcx@gmail.com>
2023-03-27 21:44:51 -0300

    config app.js

--- app.js
+++ app.js
@@ -3,15 +3,13 @@ const fs = require("fs");
 const content = new Date().toLocaleString().concat("\n");
 const time = new Date().toLocaleString();
 const fileName = "date.txt";
-const message = "auto"
+const message = "auto";
 
 fs.appendFile(fileName, content, (cb) => {
   exec("git add ".concat(fileName), (err, stdout, stderr) => {
     exec("git commit -m ".concat(message), (err, stdout, stderr) => {
-      exec("git push main", (err, stdout, stderr) => {
-        exec("git status", (err, stdout, stderr) => {
-          console.log(stdout);
-        });
+      exec("git push", (err, stdout, stderr) => {
+        console.log('done')
       });
     });
   });
