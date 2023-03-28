cb917ab5e2cab0e4930cd9e4b5247c56042d5a85
croco <adrianomxcx@gmail.com>
2023-03-27 21:32:29 -0300

    config

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const { exec } = require("child_process");
 const fs = require("fs");
+const content = new Date().toLocaleString().concat("\n");
 const time = new Date().toLocaleString();
 const fileName = "date.txt";
-const message = "message: ".concat(time);
+const message = "auto"
 
-fs.writeFile(fileName, time, (cb) => {
-  exec("git status", (err, stdout, stderr) => {
-    console.log(stdout);
-    exec("git add ".concat(fileName), (err, stdout, stderr) => {
-      console.log(stdout);
-      exec("git commit -m ".concat(message), (err, stdout, stderr) => {
-        console.log(stdout);
+fs.appendFile(fileName, content, (cb) => {
+  exec("git add ".concat(fileName), (err, stdout, stderr) => {
+    exec("git commit -m ".concat(message), (err, stdout, stderr) => {
+      exec("git push main", (err, stdout, stderr) => {
+        exec("git status", (err, stdout, stderr) => {
+          console.log(stdout);
+        });
       });
     });
   });
